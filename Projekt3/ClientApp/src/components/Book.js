f0b05b18744a67b7d466392ee5e10299ecbee228
Rafal <rafciu10@gmail.com>
2019-05-28 17:17:15 +0200

    lista ksiazek

---
+++ Projekt3/ClientApp/src/components/Book.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+
+export default class Book extends Component {
+
+
+    render() {
+        return (
+            <tr>
+                <th scope="row">{this.props.index}</th>
+                <td>{this.props.author}</td>
+                <td>{this.props.title}</td>
+                <td>{this.props.year}</td>
+            </tr>
+        );
+    }
+}
---
+++ Projekt3/ClientApp/src/components/BookList.js
@@ -0,0 +1,50 @@
+﻿import React, { Component } from 'react';
+import Book from './Book.js';
+
+export default class BookList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { books: []}
+    }
+
+
+    componentWillMount() {
+
+        fetch('https://localhost:44371/api/books', {
+            method: 'GET',
+            body: JSON.stringify(),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(response => {
+             this.setState({ books:response })
+            })
+            .catch(error => console.error('Error:', error))
+    }
+
+    render() {
+        return (      
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.books.map((item, index) => <Book author={item.author} title={item.title} year={item.year} index={index + 1} />)
+
+                    }
+                </tbody>
+            </table>
+        );
+    }
+}
+
+
